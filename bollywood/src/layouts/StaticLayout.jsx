ca1f0b93d201fe30e6f1445ea4006495cbd9fc23
Akash1816 <22btad004@kahedu.edu.in>
2025-05-31 10:42:26 +0000

    lets get started

---
+++ bollywood/src/layouts/StaticLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+//import { link, outlet } from 'react-router-dom';
+
+
+export default function StaticLayout() {
+    return (
+        <>
+            <header className="fixed top-0 bg-red-500 w-screen h-16 flex justify-center items-center py-10">
+
+                <div className="text-2xl font-bold font-fantasy shadow-black  ">
+                    MOVIEBOXED
+                </div>
+                
+            </header>
+        </>
+    )
+}
